d61191b626207ab7c9e2f40d98765dfdb7eb9035
Crisila Aban <Crisila@MacBook-Pro-Alisirc.local>
2023-03-19 13:16:28 -0700

    Added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -65,6 +65,21 @@ router.get('/:id', (req, res) => {
   }
 })
 
+// -------------------EDIT-----------------------
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id] })
+  }
+})
+
+
 // -------------DELETE----------------------
 
 router.delete('/:id', (req, res) => {
